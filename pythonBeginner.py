18da32156907e7f33fdd50231e8d95ec3d94d0f3
biancaPricopi <pb31627@gmail.com>
2022-08-27 16:53:46 +0300

    tuples=immutable lists

--- pythonBeginner.py
+++ pythonBeginner.py
@@ -1,9 +1,2 @@
-x = [4, True, 'hi']
-x.extend([1, 3, 4])
-x.pop(0)
-# x si y pointeaza la aceeasi referinta so daca modific x, y o sa aiba si el modificarea
-# y = x[:] ca sa fac o copie a lui x
-y = x
-x[0] = 'eh'
-print('x=', x)
-print('y=', y)
+x = (0, 0, 2, 2)
+print(x[1])
